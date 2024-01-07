873d5892f2b365b171199c0d1ff776968e8b0b68
Favas Cherukunnu <mhdfavascheru@gmail.com>
2024-01-08 01:05:57 +0530

    added timer and game over

--- script.js
+++ script.js
@@ -1,12 +1,21 @@
-let topB = 0, leftB = 0, bottomB = 0;
 const step = 57;
 const boxHeight = 42
 const boxWidth = 42
 const foodwidth = 30;
 const foodHeight = 30;
 const boxRef = document.getElementById('box')
+const topSection = document.getElementById('topSection');
+const playGround = document.getElementById('playGround');
+const secondElement = document.getElementById('second');
+const errorToast = document.getElementById('errorMessage')
+let second = 5
+boxRef.style.top = (topSection.offsetHeight + 'px')
+let topB = topSection.offsetHeight, leftB = 0;
 let rotation = 90;
-let score=0
+let score = 0;
+let secondstep = 5;
+let gameStarted = false
+secondElement.innerHTML = second;
 
 
 const food = document.createElement('div')
@@ -63,8 +72,8 @@ function moveTo(diretion) {
     if (leftB < 0) {
         leftB = 0
     }
-    if (topB < 0) {
-        topB = 0
+    if (topB < topSection.offsetHeight) {
+        topB = topSection.offsetHeight
     }
 
 
@@ -78,7 +87,7 @@ function moveTo(diretion) {
 
 
 let lefTimer, rightTimer, topTimer, bottomTimer;
-let statusTimer
+let statusTimer, holdedTimout
 let controllKeystatus = {}
 
 function isAnyControllKeyPressed() {
@@ -96,71 +105,49 @@ function isAnyControllKeyPressed() {
 
 document.addEventListener('keydown', function (event) {
     // clearTimeout(statusTimer)
-    if (isAnyControllKeyPressed()) {
-        switch (event.code) {
-            case 'ArrowUp':
-                if (!topTimer) {
-                    topTimer = setInterval(
-                        () => moveTo('top'),
-                        50
-                    )
-                }
-                break;
-            case 'ArrowRight':
-                if (!rightTimer) {
-                    rightTimer = setInterval(
-                        () => moveTo('right'),
-                        50
-                    )
-                }
-                break;
-            case 'ArrowLeft':
-                if (!lefTimer) {
-                    lefTimer = setInterval(
-                        () => moveTo('left'),
-                        50
-                    )
-                }
-                break;
-            case 'ArrowDown':
-                if (!bottomTimer) {
-                    bottomTimer = setInterval(
-                        () => moveTo('bottom'),
-                        50
-                    )
-                }
-                break;
-            // Add more cases if needed for other keys
-        }
-    } else {
-        switch (event.code) {
-            case 'ArrowUp':
-                moveTo('top')
-                break;
-            case 'ArrowRight':
-                moveTo('right')
-                break;
-            case 'ArrowLeft':
-                moveTo('left')
-                break;
-            case 'ArrowDown':
-                moveTo('bottom')
-                break;
-            // Add more cases if needed for other keys
+
+    controllKeystatus[event.code] = true
+    if (controllKeystatus['ArrowUp']) {
+        if (!topTimer) {
+            topTimer = setInterval(
+                () => moveTo('top'),
+                50
+            )
         }
     }
-    controllKeystatus[event.code] = true
+    if (controllKeystatus['ArrowRight']) {
+        if (!rightTimer) {
+            rightTimer = setInterval(
+                () => moveTo('right'),
+                50
+            )
+        }
+    }
+    if (controllKeystatus['ArrowLeft']) {
+        if (!lefTimer) {
+            lefTimer = setInterval(
+                () => moveTo('left'),
+                50
+            )
+        }
+    }
+    if (controllKeystatus['ArrowDown']) {
+        if (!bottomTimer) {
+            bottomTimer = setInterval(
+                () => moveTo('bottom'),
+                50
+            )
+        }
+    }
+    // Add more cases if needed for other keys
+
 
 });
 
 document.addEventListener('keyup', function (event) {
     // clearTimeout(statusTimer)
-    statusTimer = setTimeout(
-        () => {
-            controllKeystatus[event.code] = false
-        },
-        100
-    )
+    controllKeystatus[event.code] = false
+
     switch (event.code) {
         case 'ArrowUp':
             clearInterval(topTimer)
@@ -190,12 +177,13 @@ var foodLeft = window.innerWidth / 2;
 var foodTop = window.innerHeight / 2;
 
 
-function showBuggInRandom (){
+function showBuggInRandom() {
     var min = 1;
+    var minH = topSection.offsetHeight;
     var maxW = window.innerWidth - 30;
     var maxH = window.innerHeight - 30;
     foodLeft = Math.floor(Math.random() * (maxW - min + 1)) + min;
-    foodTop = Math.floor(Math.random() * (maxH - min + 1)) + min;
+    foodTop = Math.floor(Math.random() * (maxH - minH + 1)) + minH;
     const food = document.getElementById('food');
     food.style.transitionProperty = 'opacity'
     food.style.visibility = 'visible'
@@ -230,7 +218,7 @@ function disAppearBugg() {
 
 
 
-setInterval(
+const positionTimer =  setInterval(
     () => {
         //check food eaten
         const foodRight = foodLeft + foodwidth;
@@ -239,12 +227,19 @@ setInterval(
         const rightB = leftB + boxWidth;
 
         if ((topB < foodTop && leftB < foodLeft && bottomB > foodTop && rightB > foodLeft) || (topB < foodTop && bottomB > foodTop && rightB > foodRight && leftB < foodRight) || (topB < foodBottom && bottomB > foodBottom && rightB > foodLeft && leftB < foodLeft) || (topB < foodBottom && bottomB > foodBottom && rightB > foodRight && leftB < foodRight)) {
+            if (gameStarted === false) {
+                startTimer()
+            }
+            gameStarted = true;
+
             disAppearBugg()
             setTimeout(
-                showBuggInRandom,500
+                showBuggInRandom, 500
             )
             displayBugg(5000)
             score++;
+            second += secondstep
+            secondElement.innerHTML = second
             document.getElementById('score').innerHTML = score
 
         }
@@ -253,6 +248,48 @@ setInterval(
         // console.log(boxRef.offsetLeft, boxRef.offsetTop)
     }, 1
 )
+
+let secondDuration = 1000
+//time
+let secondTimer;
+
+function startTimer() {
+
+    secondTimer = setTimeout(
+        () => {
+            if (second <= 0) {
+                gameStarted = false;
+                clearInterval(positionTimer)
+                clearInterval(foodTimer)
+                errorToast.style.zIndex = 50;
+                errorToast.style.transition = '1s all'
+                errorToast.style.transform = 'scale(1)'
+            }
+            if (gameStarted) {
+                second--;
+                secondElement.innerHTML = second
+                startTimer()
+            }
+
+        }, secondDuration
+    )
+
+}
+
+
+//dicreaseTime
+setInterval(
+    () => {
+        if (secondDuration > 300)
+            secondDuration -= 50;
+        else
+            secondDuration = 300
+
+    }, 10000
+)
+
+//Error element
+
 
 
 window.addEventListener('resize', moveTo)
